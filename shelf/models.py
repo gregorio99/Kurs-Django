8b62b66d79983e7d9eeb1b9efecb6b0a6f17afd8
gregorio99 <gregorio99@gmail.com>
2015-06-26 21:39:04 +0200

    spotkanie 12.11.2014

--- shelf/models.py
+++ shelf/models.py
@@ -5,9 +5,16 @@ class Author(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return "{first_name} {last_name}".format(first_name=self.first_name,
+                                                 last_name=self.last_name)
+
 
 class Publisher(models.Model):
     name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -15,3 +22,6 @@ class Book(models.Model):
     author = models.ForeignKey(Author)
     isbn = models.CharField(max_length=17)
     publisher = models.ForeignKey(Publisher)
+
+    def __str__(self):
+        return self.title
